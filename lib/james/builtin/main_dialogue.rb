81c8a43650ad0873af7148b34cf0a00810bc7b1b
Florian Hanke <florian.hanke@gmail.com>
2011-05-09 02:25:51 +1000

    + builtin dialogue for controlling

--- lib/james/builtin/main_dialogue.rb
+++ lib/james/builtin/main_dialogue.rb
@@ -1,20 +1,25 @@
-James.dialogue do
+class Bla
+
+  include James::Dialogue
 
   hear 'James?' => :awake
 
   state :awake do
-    hear 'Sleep, James, sleep' => :sleeping
+    hear 'Leave me alone, James' => :away
     into { "Sir?" }
   end
 
-  state :sleeping do
-    hear 'James, are you there?' => :awake,
-         'Exit, please'          => :exit
+  state :away do
+    hear 'Are you there, James?' => :awake,
+         "That's it for today, James" => :exit
     into { "Goodbye, Sir" }
   end
 
   state :exit do
-    into { Kernel.exit }
+    into do
+      puts "James: Exits through a side door."
+      Kernel.exit
+    end
   end
 
 end
