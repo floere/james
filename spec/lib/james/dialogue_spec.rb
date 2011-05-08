2963ff80e0262658131aefc6b03022940d250448
Florian Hanke <florian.hanke@gmail.com>
2011-05-09 09:58:01 +1000

    ! dialogue spec

--- spec/lib/james/dialogue_spec.rb
+++ spec/lib/james/dialogue_spec.rb
@@ -1,6 +1,7 @@
 # encoding: utf-8
 #
-require File.expand_path '../../../../lib/james/dialogue', __FILE__
+require File.expand_path '../../../../lib/james/dialogue_api', __FILE__
+require File.expand_path '../../../../lib/james/dialogue_internals', __FILE__
 
 describe James::Dialogue do
   
