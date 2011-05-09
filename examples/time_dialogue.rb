c635e82753929ce7626c79aa69a5d530d88ba002
Florian Hanke <florian.hanke@gmail.com>
2011-05-09 22:35:39 +1000

    + examples/time_dialogue.rb, + README

---
+++ examples/time_dialogue.rb
@@ -0,0 +1,27 @@
+# If using the gem, replace with: require 'james'
+#
+require File.expand_path '../../lib/james', __FILE__
+
+# Time dialogue by Florian Hanke.
+#
+# This is a very simple James example.
+#
+# It has only one state, :time, and a single
+# entry/hook phrase, "What time is it?"
+#
+# How could you enhance it to answer "What date is it?" ?
+#
+class TimeDialogue
+
+  include James::Dialogue
+
+  hear 'What time is it?' => :time
+  state :time do
+    hear ['What time is it?', 'What time is it now?'] => :time
+    into do
+      time = Time.now
+      "It is currently #{time.hour} #{time.min}."
+    end
+  end
+
+end
