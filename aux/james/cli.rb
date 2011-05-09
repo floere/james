d276d050c04de2a14b93f455ab68c9eb85c606b9
Florian Hanke <florian.hanke@gmail.com>
2011-05-09 18:09:48 +1000

    + restructuring

--- aux/james/cli.rb
+++ aux/james/cli.rb
@@ -4,17 +4,25 @@ module James
 
   class CLI
 
-    def execute *dialogues
-      all_dialogues = Dir["**/*_dialog{,ue}.rb"]
-      all_dialogues.select! { |dialogue| dialogues.any? { |given| dialogue =~ %r{#{given}_dialog(ue)?.rb$} } } unless dialogues.empty?
+    def execute *given_dialogues
+      dialogues = find_dialogues
+      dialogues.select! { |dialogue| given_dialogues.any? { |given| dialogue =~ %r{#{given}_dialog(ue)?.rb$} } } unless given_dialogues.empty?
 
-      puts "James: Using #{all_dialogues.join(', ')} for our conversation, Sir."
+      puts "James: I haven't found anything to talk about (No *_dialog{ue,}.rb files found). Exiting." or exit!(1) if dialogues.empty?
 
-      all_dialogues.each do |dialogue|
-        require File.expand_path dialogue, Dir.pwd
-      end
+      puts "James: Using #{dialogues.join(', ')} for our conversation, Sir."
+
+      require_all dialogues
 
       James.listen
+    end
+    def find_dialogues
+      Dir["**/*_dialog{,ue}.rb"]
+    end
+    def require_all dialogues
+      dialogues.each do |dialogue|
+        require File.expand_path dialogue, Dir.pwd
+      end
     end
 
   end
