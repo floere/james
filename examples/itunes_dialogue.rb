6c58a36f3ca2f53a021d9206fb2196eb6f073b73
Florian Hanke <florian.hanke@gmail.com>
2011-05-11 09:15:01 +1000

    + itunes dialogue

---
+++ examples/itunes_dialogue.rb
@@ -0,0 +1,25 @@
+# If using the gem, replace with: require 'james'
+#
+require File.expand_path '../../lib/james', __FILE__
+
+class ItunesDialogue
+
+  include James::Dialogue
+
+  hear 'Open itunes' => :itunes
+  state :itunes do
+    hear 'Next track' => ->() do
+      `osascript -e 'tell application "iTunes"' -e "next track" -e "end tell"`
+      "Playing next track, Sir."
+    end
+    hear 'Previous track' => ->() do
+      `osascript -e 'tell application "iTunes"' -e "previous track" -e "end tell"`
+      "Playing previous track, Sir."
+    end
+    into do
+      `osascript -e 'tell application "iTunes" to play'`
+      "Opening i Tunes, Sir."
+    end
+  end
+
+end
