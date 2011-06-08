1a3e42150757e18dfaa70315db61d56a60941bdd
Florian Hanke <florian.hanke@gmail.com>
2011-06-08 17:41:07 +1000

    + Audio waits until the last audio has been finished

--- lib/james/outputs/audio.rb
+++ lib/james/outputs/audio.rb
@@ -15,7 +15,12 @@ module James
 
       # Say the given text out loud.
       #
+      # Waits for the last text to be finished
+      #
       def say text
+        while @output.isSpeaking
+          sleep 0.1
+        end
         @output.startSpeakingString text
       end
 
