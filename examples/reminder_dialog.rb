28e0dc580bce610c5b9dd55ab905cc4b703a0144
Florian Hanke <florian.hanke@gmail.com>
2011-06-28 15:53:29 +1000

    + correctly pluralize the hours in recognition and speech

--- examples/reminder_dialog.rb
+++ examples/reminder_dialog.rb
@@ -4,25 +4,34 @@
 # require 'james'
 require File.expand_path '../../lib/james', __FILE__
 
+module PluralizedHour
+  def pluralized_hour i
+    "#{i} hour#{ 's' if i > 1 }"
+  end
+end
+
 # Simple reminder dialog by Florian Hanke.
 #
 James.dialog do
+  include PluralizedHour
 
   hear 'Set a reminder' => :reminder
-
+  
   state :reminder do
+    extend PluralizedHour
+    
     10.downto(1).each do |i|
-      hear "Remind me in #{i} hours" => (->() do
+      hear "Remind me in #{pluralized_hour(i)}" => (->() do
         # Run a thread that sleeps i hours.
         #
         Thread.new do
           sleep i*3600
-          James.controller.say "Hi, this is James, you set a reminder #{i} hours ago, so I am reminding you! "*3
+          James.controller.say "Hi, this is James, you set a reminder #{pluralized_hour(i)} ago, so I am reminding you! "*3
         end
 
         # Return affirmative message.
         #
-        "Ok, I'll remind you in #{i} hours."
+        "Ok, I'll remind you in #{pluralized_hour(i)}."
       end)
     end
     into { "In how many hours should I remind you?" }
